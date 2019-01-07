920df9f9f5fb518362a1493f454c86d51bc9c2b6
Your Name <valentiu.king@gmail.com>
2019-01-07 21:32:36 +0200

    load contacts from json

---
+++ public/index.js
@@ -0,0 +1,23 @@
+function loadContacts() {
+    $.ajax('data/contacts.json').done(function(contacts){
+        console.info('contacts loaded', contacts);
+        displayContacts(contacts);
+    });
+}
+
+function displayContacts(contacts) {
+    var rows = contacts.map(function(contact){
+        console.log("transofrm contacts", contact);
+        return`<tr>
+            <td>${contact.fistName}</td>
+            <td>${contact.lastName}</td>
+            <td>${contact.phone}</td>
+            <td><a href="data/contacts.json">x</a></td>
+        </tr>`;
+    });
+
+    console.warn('rows', rows);
+    document.querySelector('tbody').innerHTML = rows.join('');
+}
+
+loadContacts();
