37cac8c7129a3d1835da61b22c5b1858394f9bbb
Your Name <valentiu.king@gmail.com>
2019-01-26 15:48:56 +0200

    add update

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,21 +3,21 @@ var fs = require('fs');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
 // /contacts/delete?phone=1234
-router.get('/delete', function(req, res, next) {
+router.get('/delete', function (req, res, next) {
   var phone = req.query.phone;
-  
+
   var content = fs.readFileSync('public/data/contacts.json');
   var contacts = JSON.parse(content);
 
-  var remainingContacts = contacts.filter(function(contact){
+  var remainingContacts = contacts.filter(function (contact) {
     return contact.phone != phone;
   });
-  
+
   content = JSON.stringify(remainingContacts, null, 2);
   fs.writeFileSync('public/data/contacts.json', content);
 
@@ -27,25 +27,54 @@ router.get('/delete', function(req, res, next) {
 });
 
 // /contacts/create
-router.post('/create', function(req, res, next) {
+router.post('/create', function (req, res, next) {
   var firstName = req.body.firstName;
   var lastName = req.body.lastName;
   var phone = req.body.phone;
-  
+
   var content = fs.readFileSync('public/data/contacts.json');
   var contacts = JSON.parse(content);
 
- contacts.push({
-  firstName,
-  lastName,
-  phone
-});
-  
+  contacts.push({
+    firstName,
+    lastName,
+    phone
+  });
+
   content = JSON.stringify(contacts, null, 2);
   fs.writeFileSync('public/data/contacts.json', content);
 
-  res.json({success: true});
-  
+  res.json({ success: true });
+
+
+});
+
+// /contacts/update
+router.post('/update', function (req, res, next) {
+  var oldPhone = req.query.phone;
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var phone = req.body.phone;
+
+  var content = fs.readFileSync('public/data/contacts.json');
+  var contacts = JSON.parse(content);
+
+  // update...(v1)
+  var contact = contacts.find(function (contact) {
+    return contact.phone == oldPhone;
+  })
+
+  //TODO update... (V2)
+
+  contact.firstName = firstName;
+  contact.lastName = lastName;
+  contact.phone = phone;
+
+  content = JSON.stringify(contacts, null, 2);
+  fs.writeFileSync('public/data/contacts.json', content);
+
+  res.json({ success: true });
+
 
 });
 
